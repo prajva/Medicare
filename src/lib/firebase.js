5632d50898fb041711955b0438973ad8b4f1cc8a
prajva <bhavyakedlaya@gmail.com>
2026-09-11 16:20:04 +0530

    Update Firebase config with measurementId and analytics support

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,16 +1,29 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
+import { getAnalytics, isSupported } from 'firebase/analytics'
 
+// Firebase configuration for MediCare Plus
 const firebaseConfig = {
-  apiKey:            "AIzaSyAJODOFtKFk5rcYaeiQ69fy6K-wmoNYC30",
-  authDomain:        "medicare-92206.firebaseapp.com",
-  projectId:         "medicare-92206",
-  storageBucket:     "medicare-92206.firebasestorage.app",
+  apiKey: "AIzaSyAJODOFtKFk5rcYaeiQ69fy6K-wmoNYC30",
+  authDomain: "medicare-92206.firebaseapp.com",
+  projectId: "medicare-92206",
+  storageBucket: "medicare-92206.firebasestorage.app",
   messagingSenderId: "164881617007",
-  appId:             "1:164881617007:web:c38811d533d885662cbd9a",
+  appId: "1:164881617007:web:c38811d533d885662cbd9a",
+  measurementId: "G-SYZS0D0H7W"
 }
 
-const app  = initializeApp(firebaseConfig)
+const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
 export const db   = getFirestore(app)
+
+// Initialize Firebase Analytics if supported in the browser environment
+export let analytics = null
+if (typeof window !== 'undefined') {
+  isSupported().then(supported => {
+    if (supported) {
+      analytics = getAnalytics(app)
+    }
+  }).catch(() => {})
+}
